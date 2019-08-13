1e4fa1276f1d5dca9931141a0b42e1287c7379ab
Delali Kumapley <delalikumapley@Apples-MacBook-Pro-39.local>
2019-08-13 14:00:03 -0400

    Completed Iteration 1c

--- ViewFinder/AddPhotoViewController.swift
+++ ViewFinder/AddPhotoViewController.swift
@@ -21,5 +21,16 @@ class AddPhotoViewController: UIViewController, UIImagePickerControllerDelegate,
         imagePicker.sourceType = .camera
         present(imagePicker, animated: true, completion: nil)
         
+        
     }
+        @IBOutlet weak var displayImage: UIImageView!
+    
+    private func imagePickerController(_picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]){
+        
+        if let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            displayImage.image = selectedImage
+        }
+      imagePicker.dismiss(animated: true, completion: nil)
 }
+}
+
