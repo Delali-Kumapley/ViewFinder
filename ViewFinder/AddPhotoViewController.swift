e05571d5a502eeb00759b56301411acaa16d0593
Delali Kumapley <delalikumapley@Apples-MacBook-Pro-39.local>
2019-08-13 13:28:56 -0400

    Iteration 2 Completed

---
+++ ViewFinder/AddPhotoViewController.swift
@@ -0,0 +1,21 @@
+//
+//  AddPhotoViewController.swift
+//  ViewFinder
+//
+//  Created by Delali Kumapley on 8/13/19.
+//  Copyright © 2019 Delali Kumapley. All rights reserved.
+//
+
+import UIKit
+
+class AddPhotoViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    var imagePicker = UIImagePickerController()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    imagePicker.delegate = self
+
+    
+    }
+}
